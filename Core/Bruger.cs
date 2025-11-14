bb0e38aadc3164e7010def77df830e82ed880672
Nikolajlovbjerg <lovbjergr12@gmail.com>
2025-11-14 11:30:40 +0100

    Model classes

---
+++ Core/Bruger.cs
@@ -0,0 +1,14 @@
+﻿namespace Core;
+
+public class Bruger
+{
+    public int BrugerId { get; set; } 
+    
+    public string Name { get; set; } 
+    
+    public string Email { get; set; } 
+    
+    public string Phone { get; set; } 
+    
+    public string Address { get; set; }
+}
