2ff4e7acb65b318114e77748ffbed67681850e78
Sommer4444 <eaa24ksom@students.eaaa.dk>
2025-11-20 18:23:23 +0100

    Kommentering på mine annoncer og tilføj annoncer

--- Server/Repositories/AnnonceRepositoryMongoDb.cs
+++ Server/Repositories/AnnonceRepositoryMongoDb.cs
@@ -1,39 +1,50 @@
-using MongoDB.Driver;
-using Core;
-using Server.PW1;
+using MongoDB.Driver; // Importerer MongoDB-driveren for at interagere med MongoDB-databasen
+using Core; // Importerer kernefunktionaliteten, herunder Annonce-klassen
+using Server.PW1; // Importerer PASSWORD-klassen for at få adgang til adgangskoden
 
 namespace Server.Repositories;
 
+//Implementerer IAnnonceRepository interfacet for at håndtere annonce-data i MongoDB
 public class AnnonceRepositoryMongoDb : IAnnonceRepository
 {
+//Repræsentation af MongoDB-collectionen for annoncer
     private readonly IMongoCollection<Annonce> aAnnonce;
 
+//Konstrukter der opretter forbindelse til MongoDB og initialiserer aAnnonce-collectionen
     public AnnonceRepositoryMongoDb()
     {
+//Opretter MongoDB Uri med brugernavn og adgangskode
         var mongoUri = $"mongodb+srv://tobiaskring111_db_user:{PASSWORD.superHemligPassword}@dbtest.adqud16.mongodb.net/";
-        var client = new MongoClient(mongoUri);
-        var database = client.GetDatabase("Genbrug");
-        aAnnonce = database.GetCollection<Annonce>("Annonce");
+        var client = new MongoClient(mongoUri); // Opretter en MongoDB-klient
+        var database = client.GetDatabase("Genbrug"); // Får adgang til "Genbrug" databasen
+        aAnnonce = database.GetCollection<Annonce>("Annonce"); //Vælger "Annonce" collectionen
     }
 
+//Henter alle annoncer fra databasen
     public List<Annonce> GetAll()
     {
+//Finder alle dokumenter i aAnnonce-collectionen og returnerer dem som en liste
         return aAnnonce.Find(_  => true).ToList();
     }
 
+//Henter en annonce baseret på dens AnonnceId
     public void Add(Annonce annonce)
     {
+//Finder den sidste annonce for at bestemme det næste AnonnceId
         var lastAnnonce = aAnnonce
             .Find(Builders<Annonce>.Filter.Empty)
             .SortByDescending(a => a.AnonnceId)
             .Limit(1)
             .FirstOrDefault();
 
+//Tildeler +1 til det sidste AnonnceId eller 1 hvis der ikke er nogen annoncer
         annonce.AnonnceId = (lastAnnonce?.AnonnceId ?? 0) + 1;
 
+//Indsætter den nye annonce i aAnnonce-collectionen
         aAnnonce.InsertOne(annonce);
     }
     
+//Opdaterer en eksisterende annonce i databasen
     public void Update(Annonce annonce)
     {
         // Finder det dokument i databasen, hvor AnonnceId matcher det annonce-objekt vi vil opdatere
@@ -42,10 +53,9 @@ public class AnnonceRepositoryMongoDb : IAnnonceRepository
         aAnnonce.ReplaceOne(filter, annonce);
     }
 
+//Delete-motode til at slette en annonce baseret på dens AnonnceId
     public void Delete(int id)
     {
         aAnnonce.DeleteOne(a => a.AnonnceId == id);
     }
-    /*Dette er en delete-metode i repository’et. Den modtager et id som parameter og 
-     fortæller MongoDB at slette det dokument(annonce) i aAnnonce-collectionen, hvor AnonnceId matcher det givne id */
 }
--- Server/Repositories/IAnnonceRepository.cs
+++ Server/Repositories/IAnnonceRepository.cs
@@ -2,16 +2,20 @@ using Core;
 
 namespace Server.Repositories;
 
-// her er interfacet, bestemmer man så de metoder der skal findes.
-// derfor er det her ting, hvad man kan gøre 
+// Her er interfacet, bestemmer man så de metoder der skal findes.
+// Derfor er det her ting, hvad man kan gøre 
 // CRUD. Create,Read,Update,Delete 
 public interface IAnnonceRepository
 {
+// Henter alle annoncer
     List<Annonce> GetAll();
-    
+
+// Tilføjer ny annonce til repositoriet
     void Add(Annonce annonce);
     
+//Opdaterer en eksisterende annonce
     void Update(Annonce annonce);
 
+// Sletter en annonce baseret på dens ID
     void Delete(int id);
 }
