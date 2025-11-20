7a3b5569c95b60e0a56f448bc4b2c4d64bdecc5b
MortenHHD <eaa24mhhda@students.eaaa.dk>
2025-11-20 12:33:20 +0100

    Reapply "Needs tweaking"
    This reverts commit 01f9c841d520ee4ff6dec85639e5859f106f4ec0.

---
+++ Server/Repositories/IMineIndkøbRepo.cs
@@ -0,0 +1,10 @@
+﻿using Core;
+using System.Collections.Generic;
+
+namespace Server.Repositories;
+
+public interface IMineIndkøbRepo
+{
+    void Add(MineIndkøb køb);
+    List<MineIndkøb> GetAllByBuyerId(int buyerId);
+}
