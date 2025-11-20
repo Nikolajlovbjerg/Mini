131d4ea34bdbcb661894ef4906da5ef6bce65f43
Tobiask111 <eaa24tkrin@students.eaaa.dk>
2025-11-20 20:53:20 +0100

    more comments

--- Server/Repositories/AnnonceRepositoryMongoDb.cs
+++ Server/Repositories/AnnonceRepositoryMongoDb.cs
@@ -30,17 +30,13 @@ public class AnnonceRepositoryMongoDb : IAnnonceRepository
 //Henter en annonce baseret på dens AnonnceId
     public void Add(Annonce annonce)
     {
-//Finder den sidste annonce for at bestemme det næste AnonnceId
         var lastAnnonce = aAnnonce
             .Find(Builders<Annonce>.Filter.Empty)
             .SortByDescending(a => a.AnonnceId)
             .Limit(1)
             .FirstOrDefault();
-
-//Tildeler +1 til det sidste AnonnceId eller 1 hvis der ikke er nogen annoncer
         annonce.AnonnceId = (lastAnnonce?.AnonnceId ?? 0) + 1;
 
-//Indsætter den nye annonce i aAnnonce-collectionen
         aAnnonce.InsertOne(annonce);
 
         //Ingen filter henter alle documenter
@@ -55,16 +51,16 @@ public class AnnonceRepositoryMongoDb : IAnnonceRepository
     
     public void Update(Annonce annonce)
     {
-        // Finder det dokument i databasen, hvor AnonnceId matcher det annonce-objekt vi vil opdatere
         var filter = Builders<Annonce>.Filter.Eq(a => a.AnonnceId, annonce.AnonnceId);
-        // Erstat det gamle dokument med det nye annonce-objekt
+    
         aAnnonce.ReplaceOne(filter, annonce);
 
         //Finder det dokument hvor det id der er i DB matcher det id du vil opdatere/ændre på
-        //Og så ændre hele dokumentet med objektet 
+        //Og så ændre hele dokumentet med objektet (Erstat det gamle dokument med det nye annonce-objekt)
+
     }
 
-//Delete-motode til at slette en annonce baseret på dens AnonnceId
+    //Delete-motode til at slette en annonce baseret på dens AnonnceId
     public void Delete(int id)
     {
         aAnnonce.DeleteOne(a => a.AnonnceId == id);
